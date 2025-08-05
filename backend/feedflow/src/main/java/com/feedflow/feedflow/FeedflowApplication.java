2dfa6212f7c2cba505ed7cbeb4f75a718204d207
Padmakumar18 <padmakumar41759@gmail.com>
2025-08-05 23:08:16 +0530

    Initial commit

---
+++ backend/feedflow/src/main/java/com/feedflow/feedflow/FeedflowApplication.java
@@ -0,0 +1,13 @@
+package com.feedflow.feedflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedflowApplication.class, args);
+	}
+
+}
---
+++ backend/feedflow/src/test/java/com/feedflow/feedflow/FeedflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedflow.feedflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
